c7eaa791e8751b962bea3fed2dd806e40b856733
Kristian Morath <kristian.morath@bluewin.ch>
2022-05-13 12:35:30 +0200

    categories

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+
+import { Routes, RouterModule} from '@angular/router';
+
+// Routes
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),   // Configure Router based on routes
+    BrowserModule,
+    HttpClientModule                // import HttpClientModule
+  ],
+  providers: [ProductService],      // allows to inject class
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
