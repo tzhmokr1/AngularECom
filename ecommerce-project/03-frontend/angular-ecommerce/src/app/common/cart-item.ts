31247791ad96c0befd4ce7eb572c49dfe84bc7bb
Kristian Morath <kristian.morath@bluewin.ch>
2022-05-19 18:56:54 +0200

    cart

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/common/cart-item.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/common/cart-item.ts
@@ -5,7 +5,6 @@ export class CartItem {
   name: string;
   imageUrl: string;
   unitPrice: number;
-
   quantity: number;
 
   constructor(product: Product) {
@@ -13,8 +12,8 @@ export class CartItem {
     this.name = product.name;
     this.imageUrl = product.imageUrl;
     this.unitPrice = product.unitPrice;
-
     this.quantity = 1;
   }
+
 
 }
